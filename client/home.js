2e7edadd3f57eff33c0be946d819610f8dd92e2f
Paul Wilson <paul.i.wilson@ft.com>
2016-11-08 10:33:38 +0000

    adding homepage

--- client/home.js
+++ client/home.js
@@ -4,15 +4,15 @@ function $$(selector){
 	return [].slice.apply(document.querySelectorAll(selector));
 }
 
-function getAppStatus(tr){
-	const name = tr.dataset.appname;
-	const statusContainer = tr.querySelector('.applist__status');
+function getAppStatus(itemNode){
+	const name = itemNode.dataset.appname;
+	const statusContainer = itemNode.querySelector('.applist__status');
 	statusContainer.className.split(' ').forEach(cls => {
 		if(cls.includes('status--')){
 			statusContainer.classList.remove(cls)
 		}
 	});
-	removeMessages(name);
+	removeMessages(itemNode);
 	statusContainer.classList.add('applist__status--waiting');
 	return fetch(`/api/v1/status/${name}`, {credentials:'include'})
 		.then(response => {
@@ -26,31 +26,31 @@ function getAppStatus(tr){
 			console.dir(json);
 			statusContainer.classList.remove('applist__status--waiting');
 			statusContainer.classList.add(`applist__status--${json.overall}`);
+			if(json.messages.length){
+				itemNode.querySelector('.applist__messages').classList.add('visible');
+			}
 			for(let message of json.messages){
-				addMessage(tr, message);
+				addMessage(itemNode, message);
 			}
 		})
 }
 
-function removeMessages(name){
-	document.querySelector(`tr.applist__messages[data-appname="${name}"] tbody`).innerHTML = '';
+function removeMessages(itemNode){
+	const messages = itemNode.querySelector('.applist__messages');
+	messages.innerHTML = '';
+	messages.classList.remove('visible');
 }
 
-function addMessage(name, message){
-	const container = document.querySelector(`tr.applist__messages[data-appname="${name}"] tbody`);
-	const messageTr = document.createElement('tr');
-	const messageTd = document.createElement('td');
-	messageTd.setAttribute('colspan', '3');
-	messageTr.classList.add('applist__message');
-	messageTr.dataset.appname = tr.dataset.appname;
-	messageTd.classList.add(`status--${message.status}`);
-	messageTd.innerText = message.text + ` (${message.region})`;
-	messageTr.appendChild(messageTd);
-	container.appendChild(messageTr);
+function addMessage(itemNode, message){
+	const li = document.createElement('li');
+	li.classList.add('applist__message', `status--${message.status}`);
+	li.dataset.appname = name;
+	li.innerText = `${message.text} (${message.region})`;
+	itemNode.querySelector('.applist__messages').appendChild(li);
 }
 
 function init(){
-	const rows = $$('tr[data-appname]').slice(0);
+	const rows = $$('.applist__item[data-appname]').slice(0);
 	return co(function* (){
 		for(let row of rows){
 			yield getAppStatus(row);
